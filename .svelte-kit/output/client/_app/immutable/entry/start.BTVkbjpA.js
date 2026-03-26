67b57c73dd0b1f8316ae20009708288072fe3cf7
yahya-azeem <yahya-azeem@users.noreply.github.com>
2026-03-26 14:06:14 -0500

    Remove tracked .svelte-kit build artifacts and ignore them

--- .svelte-kit/output/client/_app/immutable/entry/start.BTVkbjpA.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BUhENwcF.js";import{w as t}from"../chunks/BRP4nv6B.js";export{t as load_css,r as start};
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CpfoXgJ4.js","_app/immutable/chunks/fwhjoy1e.js","_app/immutable/chunks/CIvPZafU.js","_app/immutable/chunks/D-D7fj7C.js","_app/immutable/chunks/9DarDDCZ.js","_app/immutable/chunks/BRP4nv6B.js","_app/immutable/chunks/C5UI2bJ6.js"];
-export const stylesheets = [];
-export const fonts = [];
